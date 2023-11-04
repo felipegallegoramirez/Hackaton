9a2c2cf65e914b47e408c1e5357b79604b9c79e7
felipe <felipegallego600@gmail.com>
2023-11-04 11:21:43 -0500

    post

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { ViewPostComponent } from './view-post/view-post.component';
 
 
 @NgModule({
@@ -16,6 +18,8 @@ import { LoginComponent } from './login/login.component';
 
     HomeComponent,
       LoginComponent,
+      CreatePostComponent,
+      ViewPostComponent,
     
   ],
   imports: [
