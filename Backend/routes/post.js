33d6ec36d66f8bd9f24a107835fe5e9571c89b73
felipe <felipegallego600@gmail.com>
2023-11-04 10:57:20 -0500

    routes

---
+++ Backend/routes/post.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const post = require("../controllers/post.controller")
+const validatorHandler  = require("../midleware/verificatorSchemas")
+const {createPostSchema , updatePostSchema , getPostSchema, createAdmonSchema} = require("../midleware/schemas/post.schema")
+const {checkAuth , checkpermision} = require('../midleware/authverify')
+
+router.get("/",checkpermision(0), post.getPosts);
+router.get("/:id",checkpermision(0),validatorHandler(getPostSchema, 'params'), post.getPost); 
+router.post(validatorHandler(createPostSchema,"body"),post.createPost)
+router.put(validatorHandler(createPostSchema,"body"),post.editPost )
+
+router.delete("/:id",checkpermision(0),validatorHandler(getPostSchema, 'params'), post.deletePost);
+
+
+module.exports = router;
