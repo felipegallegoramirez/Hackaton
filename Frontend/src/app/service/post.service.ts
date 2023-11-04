2e9cd15f418d5740df735d8db0f1fc4e23141a47
felipe <felipegallego600@gmail.com>
2023-11-04 12:05:11 -0500

    post-service

--- Frontend/src/app/service/post.service.ts
+++ Frontend/src/app/service/post.service.ts
@@ -7,25 +7,25 @@ import { Post } from '../models/post';
 })
 export class PostService {
 
-  private apiUrl = 'http://localhost:3000/api/'; 
+  private apiUrl = 'http://localhost:3000/api/post'; 
 
   constructor(private http: HttpClient) { }
 
 
 
   createPost(post: Post){
-    return this.http.post<boolean>(this.apiUrl, post);
+    return this.http.post<any>(this.apiUrl, post);
   }
 
-  getUser(id:string){
+  getPost(id:string){
     return this.http.get<Post>(this.apiUrl + `/${id}`);
   }
 
-  putUser(post: Post){
+  putPost(post: Post){
     return this.http.put(this.apiUrl + `/`, post);
   }
 
-  getUsers(){
+  getPosts(){
     return this.http.get<Post[]>(this.apiUrl);
   }
 }
